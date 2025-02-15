4fa8ac5e4e93d785362b1d3b6d16326f1ea97923
peterjp001 <jeanpierrepeterson01@gmail.com>
2025-02-15 02:16:21 -0500

    resolve type error for searchParams in product/page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,7 +3,7 @@ import ProductsContainer from '@/components/products/ProductsContainer';
 export default async function ProductsPage({
   searchParams,
 }: {
-  searchParams: { layout: string; search: string };
+  searchParams: Promise<{ layout: string; search: string }>;
 }) {
   const { layout, search } = await searchParams;
 
