e7c4fddd3009aab832f9d18921894b159af5afd6
peterjp001 <jeanpierrepeterson01@gmail.com>
2025-02-18 01:41:59 -0500

    rules problem commented

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -4,9 +4,9 @@ import { RxReload } from 'react-icons/rx';
 import { useFormStatus } from 'react-dom';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
-import { SignInButton } from '@clerk/nextjs';
-import { FaRegHeart, FaHeart } from 'react-icons/fa';
-import { LuTrash2, LuSquare } from 'react-icons/lu';
+// import { SignInButton } from '@clerk/nextjs';
+// import { FaRegHeart, FaHeart } from 'react-icons/fa';
+// import { LuTrash2, LuSquare } from 'react-icons/lu';
 
 type btnSize = 'default' | 'lg' | 'sm';
 
