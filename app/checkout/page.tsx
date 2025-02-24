0e00272349ba1dfc2f3a1e7fcc5bb5508bfa9118
peterjp001 <jeanpierrepeterson01@gmail.com>
2025-02-23 23:02:02 -0500

    suspense added to checkout page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import axios from 'axios';
 import { useSearchParams } from 'next/navigation';
 import React, { Suspense, useCallback } from 'react';
@@ -7,14 +8,13 @@ import { EmbeddedCheckoutProvider, EmbeddedCheckout } from '@stripe/react-stripe
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string);
 
-export default function CheckoutPage() {
+function CheckoutContent() {
   const searchParams = useSearchParams();
 
   const orderId = searchParams.get('orderId');
   const cartId = searchParams.get('cartId');
 
   const fetchClientSecret = useCallback(async () => {
-    // Create a Checkout Session
     const response = await axios.post('/api/payment', {
       orderId: orderId,
       cartId: cartId,
@@ -25,12 +25,18 @@ export default function CheckoutPage() {
   const options = { fetchClientSecret };
 
   return (
+    <div id='checkout'>
+      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
+  );
+}
+
+export default function CheckoutPage() {
+  return (
     <Suspense fallback={<div>Loading...</div>}>
-      <div id='checkout'>
-        <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
-          <EmbeddedCheckout />
-        </EmbeddedCheckoutProvider>
-      </div>
+      <CheckoutContent />
     </Suspense>
   );
 }
